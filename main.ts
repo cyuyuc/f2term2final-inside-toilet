f35f95d63952f7f8329b829b0e944f4738a3cef8
cyuyuc <130678477+cyuyuc@users.noreply.github.com>
2023-05-27 11:45:18 +0800

    Restore '24/5/2023 下午11:21:05 更新 pxt.json'

--- main.ts
+++ main.ts
@@ -1,25 +1,23 @@
-let availability = 0
-let flushing = 0
 let distance = 0
+let y = 0
 radio.setGroup(11)
 radio.setTransmitPower(7)
 basic.forever(function () {
+    y = 0
     distance = sonar.ping(
     DigitalPin.P2,
     DigitalPin.P12,
     PingUnit.Centimeters
     )
-})
-basic.forever(function () {
-    if (distance < 100 && flushing == 1) {
+    basic.showNumber(distance)
+    if (distance < 150) {
+        y = distance
         radio.sendValue("Unavailable", 3)
-        availability = 1
-    } else if (distance > 100 && availability == 1) {
+    } else if (y > 150) {
         radio.sendValue("Flushing", 2)
-        availability = 0
-        flushing = 1
+        music.playTone(262, music.beat(BeatFraction.Whole))
     } else {
         radio.sendValue("Available", 1)
+        y = distance
     }
-    flushing = 0
 })
