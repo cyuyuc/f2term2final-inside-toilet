8c794ea3babe4150274613c18749b3d42b96f9cd
cyuyuc <130678477+cyuyuc@users.noreply.github.com>
2023-05-27 11:42:58 +0800

    Update main.blocks, main.ts, main.py

--- main.py
+++ main.py
@@ -1,13 +1,11 @@
-distance = 0
 y = 0
+distance = 0
 radio.set_group(11)
 radio.set_transmit_power(7)
 
 def on_forever():
-    global y, distance
-    y = 0
+    global distance
     distance = sonar.ping(DigitalPin.P2, DigitalPin.P12, PingUnit.CENTIMETERS)
-    basic.show_number(distance)
 basic.forever(on_forever)
 
 def on_forever2():
@@ -17,8 +15,6 @@ def on_forever2():
         radio.send_value("Unavailable", 3)
     elif y > 150:
         radio.send_value("Flushing", 2)
-        music.play_tone(262, music.beat(BeatFraction.WHOLE))
     else:
         radio.send_value("Available", 1)
-        y = distance
 basic.forever(on_forever2)
--- main.ts
+++ main.ts
@@ -1,23 +1,25 @@
+let availability = 0
+let flushing = 0
 let distance = 0
-let y = 0
 radio.setGroup(11)
 radio.setTransmitPower(7)
 basic.forever(function () {
-    y = 0
     distance = sonar.ping(
     DigitalPin.P2,
     DigitalPin.P12,
     PingUnit.Centimeters
     )
-    basic.showNumber(distance)
-    if (distance < 150) {
-        y = distance
+})
+basic.forever(function () {
+    if (distance < 100 && flushing == 1) {
         radio.sendValue("Unavailable", 3)
-    } else if (y > 150) {
+        availability = 1
+    } else if (distance > 100 && availability == 1) {
         radio.sendValue("Flushing", 2)
-        music.playTone(262, music.beat(BeatFraction.Whole))
+        availability = 0
+        flushing = 1
     } else {
         radio.sendValue("Available", 1)
-        y = distance
     }
+    flushing = 0
 })
