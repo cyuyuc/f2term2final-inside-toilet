164ff3b50ed28af28deb7c04e0e4fb0752390bc6
cyuyuc <130678477+cyuyuc@users.noreply.github.com>
2023-05-27 11:49:48 +0800

    Update main.blocks, main.ts, main.py

--- main.py
+++ main.py
@@ -1,24 +1,24 @@
+availability = 0
+flushing = 0
 distance = 0
-y = 0
 radio.set_group(11)
 radio.set_transmit_power(7)
 
 def on_forever():
-    global y, distance
-    y = 0
+    global distance
     distance = sonar.ping(DigitalPin.P2, DigitalPin.P12, PingUnit.CENTIMETERS)
-    basic.show_number(distance)
 basic.forever(on_forever)
 
 def on_forever2():
-    global y
-    if distance < 150:
-        y = distance
+    global availability, flushing
+    if distance < 100 and flushing == 1:
         radio.send_value("Unavailable", 3)
-    elif y > 150:
+        availability = 1
+    elif distance > 100 and availability == 1:
         radio.send_value("Flushing", 2)
-        music.play_tone(262, music.beat(BeatFraction.WHOLE))
+        availability = 0
+        flushing = 1
     else:
         radio.send_value("Available", 1)
-        y = distance
+    flushing = 0
 basic.forever(on_forever2)
