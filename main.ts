6f4da3e38faf4ed8f95d4e89edd09c9343b66670
cyuyuc <130678477+cyuyuc@users.noreply.github.com>
2023-05-27 22:01:21 +0800

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,8 +1,10 @@
-let availability = 0
-let flushing = 0
 let distance = 0
+let availability = 1
 radio.setGroup(11)
 radio.setTransmitPower(7)
+robotbit.Servo(robotbit.Servos.S1, 180)
+basic.pause(500)
+robotbit.Servo(robotbit.Servos.S1, 0)
 basic.forever(function () {
     radio.sendNumber(Environment.ReadDust(DigitalPin.P16, AnalogPin.P1))
 })
@@ -12,17 +14,18 @@ basic.forever(function () {
     DigitalPin.P12,
     PingUnit.Centimeters
     )
-    if (distance < 100 && flushing == 1) {
+    if (distance <= 30) {
         radio.sendValue("Unavailable", 3)
-        availability = 1
-    } else if (distance > 100 && availability == 1) {
-        robotbit.Servo(robotbit.Servos.S1, 0)
-        radio.sendValue("Flushing", 2)
-        availability = 0
-        flushing = 1
+        availability = 2
+    } else if (distance >= 30 && availability == 2) {
         robotbit.Servo(robotbit.Servos.S1, 180)
-    } else {
+        radio.sendValue("Flushing", 2)
+        availability = 1
+        basic.pause(1000)
+        robotbit.Servo(robotbit.Servos.S1, 0)
+    } else if (distance >= 30 && availability == 1) {
         radio.sendValue("Available", 1)
+    } else {
+        radio.sendValue("Error", 4)
     }
-    flushing = 0
 })
