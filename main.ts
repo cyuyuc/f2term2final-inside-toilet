676a11e8b9c4331624ae8adf409e87755279e757
cyuyuc <130678477+cyuyuc@users.noreply.github.com>
2023-05-28 21:00:49 +0800

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,4 @@
+let sos = 0
 let distance = 0
 let availability = 1
 radio.setGroup(11)
@@ -17,14 +18,17 @@ basic.forever(function () {
     if (distance <= 70) {
         radio.sendValue("Unavailable", 3)
         availability = 2
+        sos += 0.2
     } else if (distance >= 70 && availability == 70) {
-        robotbit.Servo(robotbit.Servos.S1, 180)
         radio.sendValue("Flushing", 2)
+        robotbit.Servo(robotbit.Servos.S1, 180)
         availability = 1
+        sos = 0
         basic.pause(1000)
         robotbit.Servo(robotbit.Servos.S1, 0)
     } else if (distance >= 80) {
         radio.sendValue("Available", 1)
+        sos = 0
     } else {
         radio.sendValue("Error", 4)
     }
